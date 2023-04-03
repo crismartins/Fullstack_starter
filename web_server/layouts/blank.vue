e35c1c0354bc71fafebaf816ff25f72d753e1947
CristopherMartins <cmartins@bonadmin.com>
2023-04-03 15:09:37 -0300

    add layout

---
+++ web_server/layouts/blank.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="body__container">
+        <main>
+            <slot />
+        </main>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .body__container{
+        min-height: 100vh;
+        display: flex;
+        flex-direction: column;
+        main{
+            flex-grow: 1;
+        }
+    }
+</style>
